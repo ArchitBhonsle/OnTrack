50393115261358678ce9a52bff945c5c4393708c
AB <arpitbhat48@gmail.com>
2021-09-22 14:03:39 +0530

    reminder static page

--- app/src/main/java/com/puipuituipui/ontrack/reminders/Reminders.java
+++ app/src/main/java/com/puipuituipui/ontrack/reminders/Reminders.java
@@ -7,10 +7,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import com.puipuituipui.ontrack.R;
 
 public class Reminders extends Fragment {
+    ListView remindersList;
+    Reminder[] reminders = {
+            new Reminder("Water plants", 15),
+            new Reminder("Charge power Bank", 0),
+            new Reminder("Send in the mail", 5),
+            new Reminder("Check marks", 30),
+            new Reminder("Wash dishes", 23),
+    };
+
     public Reminders() { /* Required empty public constructor */ }
 
     public static Reminders newInstance() {
@@ -28,6 +41,11 @@ public class Reminders extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_reminders, container, false);
+        View view = inflater.inflate(R.layout.fragment_reminders, container, false);
+        remindersList = view.findViewById(R.id.reminders_list);
+        RemindersListAdapter adapter = new RemindersListAdapter(
+                view.getContext(), new ArrayList<>(Arrays.asList(reminders)));
+        remindersList.setAdapter(adapter);
+        return view;
     }
 }
