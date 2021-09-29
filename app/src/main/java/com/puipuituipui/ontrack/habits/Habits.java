fcce6e83b9cc0bc5f8937fbd80d1eeb7beab61b8
Archit Bhonsle <abhonsle2000@gmail.com>
2021-09-29 10:53:32 +0530

    Basic BottomSheetDialog for Todos and Habits

--- app/src/main/java/com/puipuituipui/ontrack/habits/Habits.java
+++ app/src/main/java/com/puipuituipui/ontrack/habits/Habits.java
@@ -1,13 +1,17 @@
 package com.puipuituipui.ontrack.habits;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.puipuituipui.ontrack.R;
 
 import java.util.ArrayList;
@@ -15,6 +19,8 @@ import java.util.Arrays;
 
 public class Habits extends Fragment {
     ListView habitsList;
+    FloatingActionButton fab;
+
     Habit[] habits = {
             new Habit("Exercise", 15),
             new Habit("Study", 0),
@@ -47,6 +53,37 @@ public class Habits extends Fragment {
                 view.getContext(), new ArrayList<>(Arrays.asList(habits)));
         habitsList.setAdapter(adapter);
 
+        fab = view.findViewById(R.id.habits_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fabClick(view.getContext());
+            }
+        });
+
         return view;
+    }
+
+    private void fabClick(Context ctx) {
+        BottomSheetDialog dialog = new BottomSheetDialog(ctx);
+        dialog.setContentView(R.layout.dialog_add_habit);
+
+        Button add = dialog.findViewById(R.id.add_habit), cancel = dialog.findViewById(R.id.cancel_habit);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Add logic
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
+
+        dialog.show();
     }
 }
--- app/src/main/java/com/puipuituipui/ontrack/todos/Todos.java
+++ app/src/main/java/com/puipuituipui/ontrack/todos/Todos.java
@@ -1,5 +1,6 @@
 package com.puipuituipui.ontrack.todos;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.puipuituipui.ontrack.R;
 
 import java.util.ArrayList;
@@ -16,6 +20,7 @@ import java.util.Arrays;
 
 public class Todos extends Fragment {
     ListView todoList;
+    FloatingActionButton fab;
     Todo[] todos = {
             new Todo(false, "AAD Literature Review"),
             new Todo(true, "AAD Text Fields and Toast"),
@@ -47,6 +52,37 @@ public class Todos extends Fragment {
                 view.getContext(), new ArrayList<>(Arrays.asList(todos)));
         todoList.setAdapter(adapter);
 
+        fab = view.findViewById(R.id.todos_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fabClick(view.getContext());
+            }
+        });
+
         return view;
+    }
+
+    private void fabClick(Context ctx) {
+        BottomSheetDialog dialog = new BottomSheetDialog(ctx);
+        dialog.setContentView(R.layout.dialog_add_todo);
+
+        Button add = dialog.findViewById(R.id.add_todo), cancel = dialog.findViewById(R.id.cancel_todo);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Add logic
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
+
+        dialog.show();
     }
 }
